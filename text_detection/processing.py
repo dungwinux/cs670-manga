06c0a623a0ec460702cd9ccef5558050ab994649
Dung Tuan Nguyen <ntddebugger@gmail.com>
2024-11-25 11:09:31 +0000

    fix: split text_detection code into module for reuse

--- text_detection/processing.py
+++ text_detection/processing.py
@@ -1,6 +1,8 @@
 from shapely import Polygon, intersection, normalize
 import numpy as np
-
+import mts
+import cv2
+import algorithm as al
 
 def convert_polygon_to_points(p):
     pts = np.array([x for x in p.exterior.coords], dtype=np.int32)
@@ -49,3 +51,88 @@ def condense(bbs):
             break
         bbs = condensed
     return condensed
+
+def convert_mask_to_points(mask):
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    polygons = []
+    for contour in contours:
+        # Reshape to (n, 2) array
+        p = contour.reshape((-1, 2))
+        polygons.append(p)
+    return polygons
+
+def dup_check(chars, isPoly=False):
+    ccs = chars if isPoly else convert_pointslist_to_polygons(chars)
+    for i, char in enumerate(ccs):
+        dup = list([i])
+        for j, char_2 in enumerate(ccs):
+            if i != j and char.equals_exact(char_2, tolerance=0.5):
+                dup.append(j) 
+        assert len(dup) == 1, "There are {} duplicate of {} at index {}: {}".format(len(dup), char, dup, [ccs[i] for i in dup])
+
+def mts_process(image_path):
+    model = mts.MTS
+    layer_agreement = 3
+    im, pred = model.process(image_path)
+    total = sum(p[0].px for p in pred)
+    total[total < layer_agreement] = 0.
+    total[total >= layer_agreement] = 1.
+    return (total, im)
+
+def cv2_process(image_path, method):
+    """Process using OpenCV algorithms. Will condense by default (TODO: remove?)"""
+    im = cv2.imread(image_path)
+    chars, chains, sep = method.process(im)
+    polys = convert_pointslist_to_polygons(chars)
+    # polys = [p.simplify(0.1) for p in polys]
+    total = condense(polys)
+    # total = polys
+    # print(total)
+    return (convert_polygons_to_pointslist(total), im)
+
+def text_detection(image_path, cv2_model):
+    # First, we use MTS to find the base mask of text
+    mask, _ = mts_process(image_path)
+    # We will build bb based on 
+    mask_np = np.array(mask[0], dtype=np.uint8) * 255
+    # print(mask_np.shape)
+    polys_cv2 = convert_mask_to_points(mask_np)
+    # print(polys_cv2)
+    # visualize_al(polys_cv2, cv2.imread(sample))
+
+    # Post-Processing 1: We use TextER in algorithm to find SFX
+    polys = convert_pointslist_to_polygons([p for p in polys_cv2 if len(p) > 3])
+    # Remove noises
+    polys = [p for p in polys if p.area > 20]
+    # visualize_al(convert_polygons_to_pointslist(polys), cv2.imread(sample))
+
+    # model.er1 = cv2.text.createERFilterNM1(model.erc1, 16, 0.00005, 0.7, 0.25, True, 0.05)
+    chars, _ = cv2_process(image_path, cv2_model)
+
+    # dup_check(chars)
+
+    marks = []
+    for char in chars:
+        # We are dropping anything beyond 10-edges
+        if len(char) > 10:
+            continue
+        overlaps = 0
+        box = convert_points_to_polygon(char)
+        for i in range(len(polys)):
+            p = polys[i]
+            if p.overlaps(box) or p.intersects(box):
+                inter = normalize(p.intersection(box))
+                if inter.area / p.area > 0.4:
+                    overlaps += 1
+        if overlaps > 0:
+            marks.append(char)
+    # visualize_cv2(mask_np)
+    new_mask = cv2.cvtColor(mask_np, cv2.COLOR_GRAY2RGB)
+    # Since all contours are disjoints, we can do in one line
+    new_draw = cv2.drawContours(new_mask, marks, -1, color=(255, 255, 255), thickness=cv2.FILLED)
+    # for i in range(len(marks)):
+    #     new_draw = cv2.drawContours(new_mask, marks, i, color=(i, 255, i), thickness=cv2.FILLED)
+    # visualize_cv2(new_draw)
+    final_mask = cv2.cvtColor(new_draw, cv2.COLOR_RGB2GRAY)
+
+    return final_mask
