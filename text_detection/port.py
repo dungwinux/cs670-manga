81787ff2f6eeb7e642d8fab319a71ab074061e64
Dung Tuan Nguyen <ntddebugger@gmail.com>
2024-12-05 03:40:13 +0000

    change: new schema to allow more info in detboxes json

---
+++ text_detection/port.py
@@ -0,0 +1,34 @@
+import cv2
+import algorithm as al
+import processing as proc
+import json
+from pathlib import Path
+
+class TextDetection:
+    def __init__(self, cwd):
+        self.cwd = Path(cwd)
+        self.input_base = cwd / 'inputs'
+        self.output_base1 = cwd / 'detboxes'
+        self.output_base2 = cwd / 'detmasks'
+        self.cv2_model = al.Text_ER()
+        self.mts_level = 2
+    def execute(self, input_relpath):
+        img_path = self.input_base / input_relpath
+        if not img_path.exists():
+            print(f"Cannot find {img_path}. ({input_base=}, {input_relpath=})")
+            return
+        img_name = str(img_path)
+        output1 = self.output_base1 / input_relpath
+        output2 = self.output_base2 / input_relpath
+        output1.parent.mkdir(parents=True, exist_ok=True)
+        output2.parent.mkdir(parents=True, exist_ok=True)
+        result = proc.text_detection(img_name, mts_level=self.mts_level, cv2_model=self.cv2_model)
+        output = {
+            "absolute_path": img_name,
+            "path": str(img_path.relative_to(self.cwd)),
+            "coordinates": result.bbs
+        }
+        with open(output1.with_suffix('.json'), 'w') as fd:
+            json.dump(output, fd)
+        # mask, grayscale, should have the size equivalent to the original image, nd.array with shape (m, n) (**NOT (m, n, 3)**)
+        cv2.imwrite(output2.with_suffix('.png'), result.mask)
